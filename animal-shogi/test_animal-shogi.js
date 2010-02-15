fdf20b445151088406d59a29ed37d0b2df40886f
User <skkmania@www1879.sakura.ne.jp>
2010-02-15 23:53:04 +0900

    try create test case for animal-shogi
    まだまだ

--- animal-shogi/test_animal-shogi.js
+++ animal-shogi/test_animal-shogi.js
@@ -10,17 +10,19 @@ var testcases = {
     var num = 1;
     assertEqual( '一', num.toKanji() );
   } },
-  testRed : function() {  with( this )  {
-    // 領域を赤色にするテスト
-    var color = 'red';
-    // ボタンをクリック(そうでなければイベントハンドラを実行)
+  testCellAfterMove : function() {  with( this )  {
+    // Pieceのmoveのテスト
+    var fromCell = game.board.cells[2][3];
+    var toCell = game.board.cells[2][2];
+    var storedPiece = fromCell.piece;
     try  {
-      new Event.simulateMouse( color, 'click' );
+      fromCell.piece.move(fromCell, toCell, false, false);
     }  catch( e )  {
       buttonClick( { target : { id : color } } );
     }
-    // 領域の背景色が'red'なら動作は正常
-    assertEqual( color, $( 'demo' ).getStyle( 'backgroundColor' ) );
+    // 駒が動いた後のセルの状態をテスト
+    assertNull(fromCell.piece); 
+    assertIdentical(storedPiece, toCell.piece);
   } },
   testGreen : function() {  with( this )  {
     /* ..... 領域を緑色にするテスト(省略) ..... */
