2b9d8e72197c92ee652b0589948b871f4e7a2dad
User <skkmania@www1879.sakura.ne.jp>
2010-03-08 11:10:01 +0900

    separate game-controller.js from animal-shogi.js
    GameControllerオブジェクトの導入。動物将棋と本将棋、その他の将棋をスイッチして実行できるように、メタクラス的に使うつもり。

--- animal-shogi/animal-shogi.js
+++ animal-shogi/animal-shogi.js
@@ -1,5 +1,4 @@
 HOST = 'http://skkmania.sakura.ne.jp/animal-shogi/';
-DisplayLevel = false;
 /**
  * common functions
  */
@@ -1230,6 +1229,446 @@ Player = Class.create({
     return 'Player: name: ' + this.name + ', isViewer: ' +  this.isViewer + ', atTop: ' + this.atTop(); 
   }
 });
+
+/**
+ * GameController
+ */
+GameController = Class.create({
+	/**
+	 * initialize(settings)
+	 */
+  initialize: function initialize(settings) {
+    this.log = new Log(Log.DEBUG, 'popup');
+    this.log.setCSSfile(HOST + "log4p.css");
+    this.log.getInto();
+    this.log.warn('start log',{'indent':1});
+    this.width = 4;  // 0 is dummy
+    this.height = 5;
+    this.settings = settings;
+    this.playingViewer = null;
+    this.log.warn('01');
+    this.container = $(settings.containerId);
+    this.log.warn('02');
+    this.controlPanel = new ControlPanel(this);
+    this.log.warn('CP created.');
+    this.board = new Board(this.container, this);
+    this.log.warn('Board created.');
+    this.blackStand = new Stand('black-stand', this);
+    this.log.warn('03');
+    this.whiteStand = new Stand('white-stand', this);
+    this.log.warn('Stand created.');
+    this.mode = 'init';
+    this.log.warn('04');
+    this.message(t('click_join_button'));
+    this.count = 0;
+       // 手数。このgameではcount手目を指した局面がthis.board, this.blackStand, this.whiteStandに反映されているものとする.
+    this.top_by_viewer = false;
+      // viewerが反転ボタンでtopを決定したとき、その値を持つ。
+      // それまではfalse. したがって、これがfalseのあいだはplayerとviewerの関係のみで
+      // topを決めることができる。
+      // すなわち、
+      //  viewer == player1 のとき、top = 0 (player1がbottomなので)
+      //  viewer == player2 のとき、top = 1 (player2がbottomなので)
+      //  viewer がplayerでないとき、top = 0 （先手がbottomがデフォルトであるので)
+    this.determineTop();
+      //  Boardのinitializeにおいてはtop=0を前提にstyle.top, style.leftを決めている
+      //  ので、topが決まったこの時点で必要なら修正しておく必要がある
+    this.board.adjust();
+      //  持ち駒の位置も決めておく
+    this.setStandPosition();
+    this.log.warn('leaving AnimalShogiGame#initialize',{'indent':-1, 'date':true,3:{'color':'green'}});
+    this.log.goOut();
+    // this.debug_dump();
+  },
+	/**
+	 * setStandPosition()
+	 */ 
+  setStandPosition: function setStandPosition() { // Game
+    $('container').style.width = 180 + (this.width)*30 + 'px';
+    if(this.top !== 1){
+      $('bottom-stand').appendChild(this.blackStand.elm);
+      $('top-stand').appendChild(this.whiteStand.elm);
+    } else {
+      $('bottom-stand').appendChild(this.whiteStand.elm);
+      $('top-stand').appendChild(this.blackStand.elm);
+    }
+    $('bottom-stand').style.height = (this.height - 1)*30 + 'px';
+    $('bottom-stand').style.margin = (this.height - 3)*30 + 'px 10px 0px 0px';
+    $('top-stand').style.height = (this.height - 1)*30 + 'px';
+    $('animal-shogi').style.height = 30 + (this.height)*30 + 'px';
+    $('animal-shogi').style.width = 80 + (this.width)*30 + 'px';
+  },
+	/**
+	 * determineTop()
+	 */
+  determineTop: function determineTop() { // Game
+this.log.getInto();
+this.log.debug('entered Game#determineTop : ', {'indent':1});
+     // 先手(player1)がbottomのとき0, top = 1 なら先手がtop
+     // はじめからtop が１になるのはplayer2がviewerのときだけ
+     // あとはviewerが反転ボタンで指定したとき
+    if (this.top_by_viewer){
+       this.top = this.top_by_viewer;
+    } else {
+      this.top = 0;  // by default
+      if (this.player2 && this.player2.isViewer) this.top = 1;
+    }
+this.log.debug('leaving determineTop with game.top : ' + this.top, {'indent':-1});
+this.log.goOut();
+  },
+	/**
+	 * setPlayer(name, opponent)
+	 */
+        // GameのsetPlayerが呼ばれるのはjoinボタンが押されたときだけ
+  setPlayer: function setPlayer(name, opponent) { // Game
+this.log.getInto();
+    if (!this.player1) {
+      this.player1 = new Player('player1', name, !opponent);
+      if (!opponent) this.playingViewer = this.player1;
+      this.controlPanel.update();
+      this.message(t('waiting'));
+      wave.getState().submitDelta({
+        player1:name
+      });
+    }
+    else if (!this.player2) {
+      if (name != this.player1.name){
+        this.player2 = new Player('player2', name, !opponent);
+        if (!opponent) this.playingViewer = this.player2;
+        this.determineTop();
+        this.controlPanel.update();
+        this.message('');
+        this.start();
+        wave.getState().submitDelta({
+          player2:name
+        });
+      } else
+        this.message(t('cannot_play_with_same_person'));
+    }
+      // TODO
+      //throw 'Invalid Player Data';
+this.log.goOut();
+  },
+	/**
+	 * message(message)
+	 */
+  message: function message(message) {
+this.log.getInto();
+    if (!this.messageElm) {
+      this.messageElm = $('message-body');
+    }
+    this.messageElm.innerHTML = message;
+this.log.goOut();
+  },
+	/**
+	 * clearMessage()
+	 */
+  clearMessage: function clearMessage() {
+    this.message('');
+this.log.goOut();
+  },
+	/**
+	 * show()
+	 */
+  show: function show() { // game
+this.log.warn('game.show');
+    //this.board.show();
+  },
+	/**
+	 * reverse()
+	 */
+  reverse: function reverse() { // game
+this.log.getInto();
+    var tmp = null;
+    this.top = (this.top === 0 ? 1 : 0);
+    this.top_by_viewer = this.top;
+    this.message('game.top became ' + this.top);
+    this.board.reverse();
+    this.board.adjust();
+    tmp = $('top-stand').childElements()[0];
+    $('top-stand').appendChild($('bottom-stand').childElements()[0]);
+    $('bottom-stand').appendChild(tmp);
+    tmp = $$('#top-stand img', '#bottom-stand img');
+    if(tmp.size() > 0){
+      tmp.invoke('toggleClassName', 'top');
+      tmp.invoke('toggleClassName', 'bottom');
+    }
+    this.controlPanel.reverse();
+this.log.goOut();
+  },
+	/**
+	 * start()
+	 */
+  start: function start() { // Game
+this.log.getInto();
+    this.log.warn('game.start was called.');
+    this.determineTop();
+    this.controlPanel.update();
+    this.board.show();
+    this.log.warn('leaving game.start.');
+this.log.goOut();
+  },
+	/**
+	 * nextTurn()
+	 */
+  nextTurn: function nextTurn() { // Game
+this.log.getInto();
+this.log.debug('entered Game#nextTurn', {'indent':1});
+    this.count++;
+    this.controlPanel.update();
+    this.clearMessage();
+    this.toggleDraggable();
+this.log.debug('leaving Game#nextTurn', {'indent':-1});
+this.log.goOut();
+  },
+	/**
+	 * getTurn()
+	 */
+  getTurn: function getTurn() {
+    // turnは論理値。countが偶数ならtrueで先手番、奇数ならfalseで後手番。
+    return (this.count % 2 == 0);
+  },
+	/**
+	 * thisTurnPlayer()
+	 */
+  thisTurnPlayer: function thisTurnPlayer() {
+    return this.getTurn() ? this.player1 : this.player2;
+  },
+	/**
+	 * isViewersTurn()
+	 */
+  isViewersTurn: function isViewersTurn() { // Game
+    return this.thisTurnPlayer().isViewer;
+  },
+	/**
+	 * finish(winner)
+	 */
+  finish: function finish(winner) {
+    this.message(winner.shortName() + t('win'));
+  },
+	/**
+	 * stateChanged()
+	 */
+  stateChanged: function stateChanged() {  // Game
+    var state = wave.getState();
+this.log.warn('stateChanged: ' + arrange(state));
+    this.fromState(state);
+this.log.warn('leaving stateChanged:');
+  },
+	/**
+	 * toString()
+	 */
+  toString: function toString() { // Game
+    var ret = '';
+    var json = this.toJSON();
+    for (var key in json) {
+      ret += key + ' : ' + json[key] + '\n';
+    }
+    return ret;
+  },
+	/**
+	 * toHTML()
+	 */
+  toHTML: function toHTML() {
+    var ret = '<table>';
+    var json = this.toJSON();
+    for (var key in json) {
+      ret += '<tr><td>' + key + '</td><td>' + json[key] + '</td></tr>\n';
+    }
+    ret += '</table>';
+    return ret;
+  },
+	/**
+	 * processPlayer(state)
+	 */
+  processPlayer: function processPlayer(state){ // Game
+this.log.getInto();
+    var viewer = wave.getViewer().getId();
+this.log.debug('entered Game#processPlayer: viewer: ' + viewer);
+    var pl1 = state.get('player1');
+    var pl2 = state.get('player2');
+    if(!pl1 && !pl2){
+this.log.debug('leaving processPlayer because state has no player.');
+this.log.goOut();
+      return;
+    }
+    if (!this.player1 && pl1) {  // 各normalスナップに1回通る
+this.log.getInto();
+this.log.debug('processPlayer: processing Player1: ');
+      var pl1IsViewer = (pl1 == viewer);
+      this.player1 = new Player('player1', pl1, pl1IsViewer );
+      this.controlPanel.update();
+this.log.info('leaving processing Player1: ');
+this.log.goOut();
+    }
+    if (!this.player2 && pl2) {  // 各normalスナップに1回通る
+this.log.getInto();
+this.log.warn('processPlayer: processing Player2: ');
+      var pl2IsViewer = (pl2 == viewer);
+      this.player2 = new Player('player2', pl2, pl2IsViewer );
+this.debug_dump();
+      this.controlPanel.update();
+this.log.warn('backed into processPlayer: processing Player2: ');
+      this.start();
+this.log.warn('leaving processPlayer: processing Player2: ');
+this.log.goOut();
+    }
+
+    if (this.player1 && this.player1.isViewer) {
+      this.playingViewer = this.player1;
+    }
+    else if (this.player2 && this.player2.isViewer) {
+      this.playingViewer = this.player2;
+    }
+    this.determineTop();
+if(this.playingViewer) this.log.debug('playingViewer is defined : ' + this.playingViewer);
+    if (this.player1 && this.player2) {
+      this.message('');
+      $('join-button').hide();
+    }
+this.log.warn('leaving Game#processPlayer: viewer: ' + viewer);
+this.log.goOut();
+  },
+        /**
+         * allPieces()
+         */
+  allPieces: function allPieces() { // game
+    return $A(this.board.cells.flatten().pluck('piece'), this.blackStand.pieces, this.whiteStand.pieces).flatten();
+  },
+	/**
+	 * fromState(state)
+	 */
+  fromState: function fromState(state) { // game
+this.log.getInto();
+this.log.warn('entered Game#fromState: ');
+    this.log.warn('<span style="color:#00FFFF">entered fromState</span>');
+    this.processPlayer(state);
+    this.count = state.get('count');
+    this.board.read(state.get('board', this.board.initialString));
+    this.blackStand.read(state.get('bstand', this.blackStand.initialString));
+    this.whiteStand.read(state.get('wstand', this.whiteStand.initialString));
+    this.toggleDraggable();
+    this.controlPanel.update();
+this.log.warn('leaving Game#fromState');
+this.log.goOut();
+  },
+	/**
+	 * toggleDraggable()
+	 */
+  toggleDraggable: function toggleDraggable(){
+this.log.getInto();
+this.log.warn('entered Game#toggleDraggable: processing drags pieces');
+    Draggables.drags.pluck('element').pluck('obj').invoke('toggleDraggable');
+this.log.warn('entered Game#toggleDraggable: processing board pieces');
+    this.board.cells.flatten().pluck('piece').compact().invoke('toggleDraggable');
+this.log.warn('Game#toggleDraggable: processing blackStand');
+    this.blackStand.pieces.invoke('toggleDraggable');
+this.log.warn('Game#toggleDraggable: processing whiteStand');
+    this.whiteStand.pieces.invoke('toggleDraggable');
+this.log.warn('leaving Game#toggleDraggable');
+this.log.goOut();
+  },
+	/**
+	 * debug_dump()
+	 */
+  debug_dump: function debug_dump(){
+    this.log.getInto({ "background":"#ff88aa","font-size":"12px" });
+    this.log.warn('debug_dump enterd', {'indent':2});
+    try{
+      var state = wave.getState();
+    } catch(e){
+      this.log.error('cannot get state : ' + e);
+    }
+    if(state)
+      this.log.warn(state.toString());
+    else
+      this.log.error('state is null');
+    var obj = {};
+    obj['all pieces']    = this.allPieces().length;
+    obj['player1']	 = (this.player1 ? this.player1.toDebugString():null);
+    obj['player2']	 = (this.player2 ? this.player2.toDebugString():null);
+    obj['playingViewer'] = (this.playingViewer ? this.playingViewer.toDebugString():null);
+    obj['top']		 = this.top;
+    //obj['board']	 = this.board.toDebugString();
+    obj['board']	 = this.board.toString();
+    obj['blackStand']	 = this.blackStand.toString();
+    obj['whiteStand']	 = this.whiteStand.toString();
+    //obj['Cell']	 = Cell.all.invoke('toDebugString').join('<br>');
+    obj['PieceOnBoard']	 = '<br>' + this.board.cells.flatten().findAll(function(c){ return c.piece != null; }).invoke('toDebugString').join('<br>');
+    obj['PieceOnBlackStand']	 = '<br>' + this.blackStand.elm.childElements().pluck('obj').invoke('toDebugString').join('<br>');
+    obj['PieceOnWhiteStand']	 = '<br>' + this.whiteStand.elm.childElements().pluck('obj').invoke('toDebugString').join('<br>');
+    obj['Droppables']	= Droppables.toDebugString();
+    obj['Draggables']	= Draggables.toDebugString();
+    for(var p in obj){
+      this.log.warn(p + ' : ' + obj[p]);
+    }
+    this.log.warn('leaving debug_dump', {'indent':-2});
+    this.log.goOut();
+  }
+});
+
+/**
+ * common functions
+ */
+	/**
+	 * sendDelta()
+	 */
+function sendDelta(){
+   // 送信
+   var delta = {};
+   delta['board'] = window.game.board.toString();
+   delta['bstand'] = window.game.blackStand.toString();
+   delta['wstand'] = window.game.whiteStand.toString();
+   delta['count'] = window.game.count.toString();
+window.game.log.warn('<div style="color:#FF0000">sending delta : </div>' + delta.toString());
+   wave.getState().submitDelta(delta);
+}
+
+	/**
+	 * moveValidate(piece, fromCell, toCell)
+	 */
+function moveValidate(piece, fromCell, toCell){
+window.game.log.getInto();
+window.game.log.debug('moveValidate entered: piece: ' + piece.toDebugString(),{'indent':1});
+   if (!window.game.isViewersTurn()) {
+     window.game.message(t('not_your_turn')); return false;
+   }
+window.game.log.debug('moveValidate 1');
+   if (toCell.piece) {
+     if (piece.isBlack() == toCell.piece.isBlack()){
+       window.game.message(t('cannot_capture_yourown_piece')); return false;
+     }
+   }
+window.game.log.debug('moveValidate 2');
+   if(!fromCell && toCell.piece) {
+     window.game.message(t('already_occupied')); return false;
+   }
+window.game.log.debug('moveValidate 3');
+   if (!piece.canMove(fromCell, toCell)) {
+     window.game.message(t('not_allowed')); return false;
+   }
+window.game.log.debug('leaving moveValidate', {'indent':-1});
+window.game.log.goOut();
+   return true;
+}
+
+	/**
+	 * checkFinish(piece, toCell)
+	 */
+function checkFinish(piece, toCell){
+window.game.log.getInto();
+window.game.log.warn('entered checkFinish');
+   var ret = (
+   // 相手のライオンを捕獲
+   (toCell.piece.type == 'lion') || 
+   // 自分のライオンが最奥に到達
+   (piece.type == 'lion' && piece.isGoal(toCell))
+           //  && window.game.isSafety(piece))
+  );
+window.game.log.warn('checkFinish leaving with : ' + ret);
+window.game.log.goOut();
+  return ret;
+}
 
 /**
  * AnimalShogiGame
