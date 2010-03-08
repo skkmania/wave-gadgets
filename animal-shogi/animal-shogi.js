97065910da4f30508db2a000609f422969f5cca7
User <skkmania@www1879.sakura.ne.jp>
2010-03-08 11:23:19 +0900

    separate common-utils.js from animal-shogi.js
    これも、いろいろな将棋に対応するための準備作業のひとつ

--- animal-shogi/animal-shogi.js
+++ animal-shogi/animal-shogi.js
@@ -1,34 +1,7 @@
 HOST = 'http://skkmania.sakura.ne.jp/animal-shogi/';
-/**
- * common functions
- */
+
 var Type2chr = { 'chick' : 'a', 'elephant' : 'b', 'giraffe' : 'c', 'lion' : 'd', 'chicken' : 'e' };
 var Chr2Type = { 'a' : 'chick', 'b' : 'elephant', 'c' : 'giraffe', 'd' : 'lion', 'e' : 'chicken' };
-
-Draggables.toDebugString = function() {
-  return '<br>' + this.drags.pluck('element').pluck('obj').invoke('toDebugString').join('<br>');
-}
-
-Droppables.toDebugString = function() {
-  return '<br>' + this.drops.invoke('toDebugString').join('<br>');
-}
-
-Array.prototype.subtract = function(ary){
-  // 配列から配列を引き算する。破壊的。
-  $A(ary).each(function(c){
-    var idx = $A(this).indexOf(c);
-    if(idx >= 0) this.splice(idx,1);
-  }.bind(this));
-}
-
-String.prototype.subtract = function(str){
-  // 文字列から文字列を引き算する
-  // 自分自身から与えられた文字列を引いた値を返す。
-  // コピーを返すので元の値は変化しない
-  var ret = $A(this);
-  ret.subtract($A(str));
-  return ret.join('');
-}
 
 function create_piece(chr){
 window.game.log.getInto();
@@ -54,18 +27,6 @@ function addDraggable(piece, startMessage){
           window.game.log.goOut();
         }.bind(piece)
       });
-}
-
-function arrange(state){
-  var str = state.toString();
-  var pattern = /\w: '.*'\,/;
-  var ret = str.replace(/\n/g, "<br>");
-  return '<div style="color: #FF0000">' + ret + '</div>';
-}
-
-Number.prototype.toKanji = function(){
-  var stock = ['','一','二','三', '四','五', '六','七', '八','九'];
-  return stock[this];
 }
 
 /**
